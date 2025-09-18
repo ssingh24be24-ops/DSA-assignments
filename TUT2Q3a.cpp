be8c3ec41b475878a4c51a9621489134f19429e1
Sahibjot Singh <ssingh24_be24@thapar.edu>
2025-09-18 14:19:31 +0530

    Update TUT2Q3a.cpp

--- TUT2Q3a.cpp
+++ TUT2Q3a.cpp
@@ -1,8 +1,9 @@
 int findMissingLinear(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         if (arr[i] != i + 1) {
-            return i + 1;  // Missing number found
+            return i + 1;  
         }
     }
-    return size + 1;  // If no mismatch, missing number is n
+    return size + 1; 
 }
+
